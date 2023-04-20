7b002730f9d6a37cfc8569f38e836669da28c079
thuyannhu <118806790+thuyannhu@users.noreply.github.com>
2023-04-20 11:35:05 +0200

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,7 +7,7 @@ use App\Model\ProductManager;
 class ProductController extends AbstractController
 {
     public function index(): string
-{
+    {
         $productManager = new ProductManager();
         $products = $productManager->selectAll('name');
 
@@ -15,7 +15,7 @@ class ProductController extends AbstractController
     }
 
     public function show(int $id): string
-{
+    {
         $productManager = new ProductManager();
         $product = $productManager->selectOneById($id);
 
@@ -23,7 +23,7 @@ class ProductController extends AbstractController
     }
 
     public function add(): ?string
-{
+    {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $product = array_map('trim', $_POST); 
             $productManager = new ProductManager();
