ea771c60350fc5be80b79ba3bfe91dc379ee5ac8
thuyannhu <118806790+thuyannhu@users.noreply.github.com>
2023-04-20 11:40:41 +0200

    Update ProductController.php
    Corrected indentation

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,7 +7,7 @@ use App\Model\ProductManager;
 class ProductController extends AbstractController
 {
     public function index(): string
-{
+    {
         $productManager = new ProductManager();
         $products = $productManager->selectAll('name');
 
@@ -15,7 +15,7 @@ class ProductController extends AbstractController
     }
 
     public function show(int $id): string
-{
+    {
         $productManager = new ProductManager();
         $product = $productManager->selectOneById($id);
 
@@ -23,9 +23,9 @@ class ProductController extends AbstractController
     }
 
     public function add(): ?string
-{
+    {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $product = array_map('trim', $_POST); 
+            $product = array_map('trim', $_POST);
             $productManager = new ProductManager();
             $id = $productManager->insert($product);
             header('Location: /products/show?id=' . $id);
@@ -33,4 +33,4 @@ class ProductController extends AbstractController
         }
         return $this->twig->render('Product/add.html.twig');
     }
- }
+}
