94b67e8b3aa9baf6842ecf713cf2feb07d16dd21
Benjamin Zilliox <benjaminzilliox@gmail.com>
2023-04-21 09:32:21 +0200

    base fonctionnalité generation du catalogue

--- src/Model/CatalogManager.php
+++ src/Model/CatalogManager.php
@@ -8,13 +8,11 @@ class CatalogManager extends AbstractManager
 {
     public const TABLE = 'products';
 
-    public function showCatalogue():array
+    public function showCatalogue(): array
     {
-    $statement = $this->pdo->prepare("SELECT name, description, price FROM " . self::TABLE . "");
-    $statement->execute();
-    $productName = $statement->fetchAll();
-    return $productName;
+        $statement = $this->pdo->prepare("SELECT name, description, price FROM " . self::TABLE . "");
+        $statement->execute();
+        $productName = $statement->fetchAll();
+        return $productName;
     }
-
 }
-
