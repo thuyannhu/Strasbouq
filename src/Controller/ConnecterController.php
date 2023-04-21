c9a088fa252bdce8f4d6f68e919efbbb889d941f
SEBASTIEN KOENIG <sebastien.koenig@orange.fr>
2023-04-21 19:31:49 +0200

    user-management

--- src/Controller/ConnecterController.php
+++ src/Controller/ConnecterController.php
@@ -6,77 +6,129 @@ use App\Model\UserManager;
 
 class ConnecterController extends AbstractController
 {
+
+    private function traduireEntree($entree){
+        $tableau = ["name" => "Le nom", "firstname" => "Le prénom", "adresse" => "L' adresse",
+        "code" => "Le code postal", "ville" => "La ville", "telephone" => "Le numéro de téléphone",
+        "email" => "L' e-mail", "password" => "Le mot de passe"];
+        return $tableau[$entree];
+    }
+
+    private function verifierNumero($errors){
+        $userM = new UserManager();
+        if (!$userM->phoneCheck($_POST['telephone'])) {
+            $errors['telephone'] = "La syntaxe du numéro de téléphone est mauvaise.";
+        }
+        return $errors;
+    }
+
+    private function verifierPasswords($errors){
+        if ($_POST['confirm'] !== $_POST['password']) {
+            $errors['confirm'] = "Les mots de passe ne correspondent pas.";
+        }
+        return $errors;
+    }
+
+    private function verifierInformation($info, $errors){
+        if (!isset($_POST[$info]) || trim($_POST[$info]) === '') {
+            $errors[$info] = $this->traduireEntree($info) . " est obligatoire.";
+        }
+        return $errors;
+    }
+
+    private function verificationGlobale(){
+        $errors = [];
+        $errors = $this->verifierInformation('name', $errors);
+        $errors = $this->verifierInformation('firstname', $errors);
+        $errors = $this->verifierInformation('adresse', $errors);
+        $errors = $this->verifierInformation('code', $errors);
+        $errors = $this->verifierInformation('ville', $errors);
+        $errors = $this->verifierInformation('telephone', $errors);
+        $errors = $this->verifierNumero($errors);
+        $errors = $this->verifierInformation('email', $errors);
+        $errors = $this->verifierInformation('password', $errors);
+        $errors = $this->verifierPasswords($errors);
+        return $errors;
+    }
+
+    private function getPassword(){
+        $username = $_POST["email"];
+        $userManager = new UserManager();
+        $userPassword = $userManager->searchUser($username, "userPassword");
+        return $userPassword;
+    }
+
+    private function isExistingUser($userPassword){
+        if (isset($userPassword[0]['userPassword'])) {
+            return True;
+        } else {
+            return False;
+        }
+    }
+
+    private function isGoodPassword($userPassword){
+        if ($_POST["password"] === $userPassword[0]['userPassword']) {
+            return True;
+        } else {
+            return False;
+        }
+    }
+
+    private function connectAgent(){
+        $userPassword = $this->getPassword();
+        if ($this->isExistingUser($userPassword)) {
+            if ($this->isGoodPassword($userPassword)) {
+                $message = ["vous etes connecté!"];
+                $alert = 'success';
+            } else {
+                $message = ["erreur!! mot de passe erroné"];
+                $alert = 'danger';
+            }
+        } else {
+            $message = ["erreur!! email inconnu"];
+            $alert = 'danger';
+        }
+        return [$message, $alert];
+    }
+
+    private function insertDataIntoDB(){
+        $userManager = new UserManager();
+        $userManager->insertUser(["firstname" => $_POST["firstname"],"lastname" => $_POST["name"],
+        "address" => $_POST["adresse"],"userPassword" => $_POST["password"],"mail" => $_POST["email"],
+        "fidelity" => 0 ,"isAdmin" => 0, "zipcode" => $_POST["code"],"city" => $_POST["ville"],
+        "phone" => $_POST["telephone"]]);
+    }
+
+    private function addErrorsToMessage($errors, $message){
+        foreach ($errors as $error) {
+            array_push($message, $error);
+        }
+        return $message;
+    }
+
     public function connect(): string
     {
         $message = [];
         $alert = "";
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (count($_POST) < 3) {
-                // Connexion compte existant
-                $username = $_POST["email"];
-                $userManager = new UserManager();
-                $userPassword = $userManager->searchUser($username, "userPassword");
 
-                if (isset($userPassword[0]['userPassword'])) {
-                    if ($_POST["password"] === $userPassword[0]['userPassword']) {
-                        $message = ["vous etes connecté!"];
-                        $alert = 'success';
-                    } else {
-                        $message = ["erreur!! mot de passe erroné"];
-                        $alert = 'danger';
-                    }
-                } else {
-                    $message = ["erreur!! email inconnu"];
-                    $alert = 'danger';
-                }
+                // Connexion compte existant
+                $resultConnexion = $this->connectAgent();
+                $message = $resultConnexion[0];
+                $alert = $resultConnexion[1];
+
             } else {
+
                 // Inscription
-                if (!isset($_POST['name']) || trim($_POST['name']) === '') {
-                    $errors['name'] = "Le nom est obligatoire";
-                }
-                if (!isset($_POST['firstname']) || trim($_POST['firstname']) === '') {
-                    $errors['firstname'] = "Le prénom est obligatoire";
-                }
-                if (!isset($_POST['adresse']) || trim($_POST['adresse']) === '') {
-                    $errors['adresse'] = "L'adresse est obligatoire";
-                }
-                if (!isset($_POST['code']) || trim($_POST['code']) === '') {
-                    $errors['code'] = "Le code postal est obligatoire";
-                }
-                if (!isset($_POST['ville']) || trim($_POST['ville']) === '') {
-                    $errors['ville'] = "La ville est obligatoire";
-                }
-                if (!isset($_POST['telephone']) || trim($_POST['telephone']) === '') {
-                    $errors['telephone'] = "Le numéro de téléphone est obligatoire";
-                }
-                $userM = new UserManager();
-                if (!$userM->phoneCheck($_POST['telephone'])) {
-                    $errors['telephone'] = "La syntaxe du numéro de téléphone est mauvaise";
-                }
-                if (!isset($_POST['email']) || trim($_POST['email']) === '') {
-                    $errors['email'] = "L'email est obligatoire";
-                }
-                if (!isset($_POST['password']) || trim($_POST['password']) === '') {
-                    $errors['password'] = "Le mot de passe est obligatoire";
-                }
-                if ($_POST['confirm'] !== $_POST['password']) {
-                    $errors['confirm'] = "Les mots de passe ne correspondent pas.";
-                }
+                $errors = $this->verificationGlobale();
 
                 if (empty($errors)) {
-                    // traitement du formulaire
-                    // puis redirection
-                    $userManager = new UserManager();
-                    $userManager->insertUser(["firstname" => $_POST["firstname"],"lastname" => $_POST["name"],
-                    "address" => $_POST["adresse"],"userPassword" => $_POST["password"],"mail" => $_POST["email"],
-                    "fidelity" => 0 ,"isAdmin" => 0, "zipcode" => $_POST["code"],"city" => $_POST["ville"],
-                    "phone" => $_POST["telephone"]]);
+                    $this->insertDataIntoDB();
                     $message = ["Bravo! Votre inscription a réussi."];
                     $alert = 'success';
                 } else {
-                    foreach ($errors as $error) {
-                        array_push($message, $error);
-                    }
+                    $message = $this->addErrorsToMessage($errors, $message);
                     $alert = 'danger';
                 }
             }
--- src/routes.php
+++ src/routes.php
@@ -20,5 +20,6 @@ return [
     'products/show' => ['ProductController', 'show', ['id']],
     'products/add' => ['ProductController', 'add',],
     'products/delete' => ['ProductController', 'delete',],
-    'userpage' => ['UserpageController', 'userpage',]
+    'userpage' => ['UserpageController', 'userpage',],
+    'connecter' => ['ConnecterController','connect',],
 ];
