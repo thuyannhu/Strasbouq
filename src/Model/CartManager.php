13cec05a034e7af199fc8b53b5511d270f950184
Benjamin Zilliox <benjaminzilliox@gmail.com>
2023-04-25 22:38:33 +0200

    suite structure panier

--- src/Model/CartManager.php
+++ src/Model/CartManager.php
@@ -7,4 +7,10 @@ use PDO;
 class CartManager extends AbstractManager
 {
     public const TABLE = 'order';
+    public const PRODUCTS = 'products';
+    public const USER = 'user';
+
+    public function showCart()
+    {
+    }
 }
