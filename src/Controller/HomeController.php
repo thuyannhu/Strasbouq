44022058078df0bf36c88b642a0e60348f0e5ea7
SEBASTIEN KOENIG <sebastien.koenig@orange.fr>
2023-05-08 08:30:33 +0200

    mise a jour

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,37 @@
 namespace App\Controller;
 
 use App\Model\ProductManager;
+use App\Controller\BouquetsController;
 
 class HomeController extends AbstractController
 {
-    /**
-     * Display home page
-     */
+    // Selects all products, their first image and adds to cart when icon is clicked
     public function index(): string
     {
         $productManager = new ProductManager();
-        $productImage = $productManager->selectAllImages('images.Products_idProducts');
+        $bouquetsController = new BouquetsController();
 
-        return $this->twig->render('Home/index.html.twig', ['images' => $productImage]);
+        //Selects all products and all images
+        $productImage = $productManager->selectAllImages();
+
+        // If icon is clicked, product is added to cart
+        if (isset($_GET['add_to_cart'])) {
+            $bouquetsController->addToCart();
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+            exit;
+        }
+
+        $newImage = [];
+        $precedent = 0;
+
+        // Checks if product image id is the same as the precedent one to avoid duplicate image
+        foreach ($productImage as $image) {
+            if ($image['Products_idProducts'] != $precedent) {
+                $newImage[] = $image;
+            }
+            $precedent = $image['Products_idProducts'];
+        }
+
+        return $this->twig->render('Home/index.html.twig', ['images' => $newImage]);
     }
 }
